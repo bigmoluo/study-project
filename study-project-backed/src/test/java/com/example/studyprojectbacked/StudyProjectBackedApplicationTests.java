d164f60790777cbfd6f495736274b6172360f29a
moluo <moluo@qq.com>
2024-03-29 21:26:03 +0800

    创建项目

---
+++ study-project-backed/src/test/java/com/example/studyprojectbacked/StudyProjectBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyprojectbacked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProjectBackedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
