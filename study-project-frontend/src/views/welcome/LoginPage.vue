7c42630b02b189269e3cee1c9977c737a6e5febc
moluo <moluo@qq.com>
2024-04-03 16:08:00 +0800

    完成前端重置页面编写

--- study-project-frontend/src/views/welcome/LoginPage.vue
+++ study-project-frontend/src/views/welcome/LoginPage.vue
@@ -62,7 +62,7 @@ function userLogin(){
                 </el-form-item>
               </el-col>
               <el-col :span="12" style="text-align: right;margin-top: 6px">
-                <el-link>忘记密码？</el-link>
+                <el-link @click="router.push('/reset')">忘记密码？</el-link>
               </el-col>
             </el-row>
             <div style="margin-top: 20px">
